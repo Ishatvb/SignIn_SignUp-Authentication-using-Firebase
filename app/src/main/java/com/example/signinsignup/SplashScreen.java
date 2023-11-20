ccdf3fd72781339ae23d2a824a7830a40e3f8743
Ishatvb <Ishatvbeohar22082003@gmail.com>
2023-11-20 23:52:18 +0530

    Open Login and Register activity from splashScreen Activity

--- app/src/main/java/com/example/signinsignup/SplashScreen.java
+++ app/src/main/java/com/example/signinsignup/SplashScreen.java
@@ -2,7 +2,10 @@ package com.example.signinsignup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+//        // Set Title
+//        getSupportActionBar().setTitle("SignUp SignIn App");
+//
+        // Open Login Activity
+        ImageView login_button = findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Open Register Activity
+        ImageView register_button = findViewById(R.id.register_button);
+
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashScreen.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
