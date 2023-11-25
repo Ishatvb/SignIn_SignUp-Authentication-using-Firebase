91151807a6205cee26aa6de3a0d614ca0dacedd8
Ishatvb <Ishatvbeohar22082003@gmail.com>
2023-11-25 22:40:09 +0530

    Bottom Navigation Implemented

--- app/src/main/java/com/example/signinsignup/MainActivity.java
+++ app/src/main/java/com/example/signinsignup/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.signinsignup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+
+                if(id == R.id.nav_home){
+                    loadFrag(new HomeFragment(), true);
+                }
+                else if(id == R.id.nav_history){
+                    loadFrag(new HistoryFragment(), false);
+                }
+                else if(id == R.id.nav_about){
+                    loadFrag(new AboutFragment(), false);
+                }
+                else if(id == R.id.nav_weather){
+                    loadFrag(new WeatherFragment(), false);
+                }
+                else if(id == R.id.nav_userprofile){
+                    loadFrag(new UserFragment(), false);
+                }
+                    return true;
+            }
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+
+    }
+    public void loadFrag(Fragment fragment, boolean flag){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if (flag){
+            ft.add(R.id.container_frame_layout, fragment);
+        }
+        else {
+            ft.replace(R.id.container_frame_layout,fragment);
+        }
+        ft.commit();
     }
 }
---
+++ app/src/main/java/com/example/signinsignup/WeatherFragment.java
@@ -0,0 +1,21 @@
+package com.example.signinsignup;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeatherFragment extends Fragment {
+    public WeatherFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+}
